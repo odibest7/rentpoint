7628c669f02ed9907ed326b1a0eccbbecd494d26
RealKingHubs <odokingsleyu@gmail.com>
2026-08-30 20:54:19 +0100

    Fix CommandError import in superuser command

--- accounts/management/commands/create_superuser_from_env.py
+++ accounts/management/commands/create_superuser_from_env.py
@@ -1,7 +1,7 @@
 import os
 
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 User = get_user_model()
 
